86324135736d9451f7aaf598446a4b243f541810
Ju102 <jlordonez02@gmail.com>
2025-10-27 14:15:49 +0100

    refactor: replace HelloWorld component with PadreDeportes and add HijoDeporte component

---
+++ src/components/HijoDeporte.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h3 style="color: darkcyan">Deporte: {{ nombredeporte }} (Child)</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HijoDeporte",
+    props: ["nombredeporte"]
+}
+</script>
+
+<style></style>
---
+++ src/components/PadreDeportes.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1 style="color: firebrick">Lista de Deportes (Parent)</h1>
+        <span v-for="deporte in deportes" :key="deporte">
+            <HijoDeporte :nombredeporte="deporte"/>
+        </span>
+    </div>
+</template>
+
+<script>
+
+import HijoDeporte from './HijoDeporte.vue'
+
+export default {
+    name: "PadreDeportes",
+    components: { HijoDeporte },
+    data() {
+        return {
+            deportes: ["Fútbol", "Baloncesto", "Tenis", "Rugby"]
+        }
+    }
+
+
+}
+</script>
+
+<style></style>
